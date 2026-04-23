919d243040d51c3af5313c273309d9def2c9629a
Filip <fanbodzo@gmail.com>
2026-04-23 12:27:54 +0200

    init project structure , dependencies , docker , postgress , parent pom.xml

---
+++ api-gateway/src/main/java/com/fanbo/api_gateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.fanbo.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
---
+++ init-db.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE db_users;
+CREATE DATABASE db_otp;
+CREATE DATABASE db_vault;
