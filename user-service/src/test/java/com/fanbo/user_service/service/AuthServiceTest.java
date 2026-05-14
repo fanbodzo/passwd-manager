91dbfc2644943d9b4016e50aaa3ed80031129978
Filip <fanbodzo@gmail.com>
2026-05-14 12:21:16 +0200

    login unit tests

--- user-service/src/test/java/com/fanbo/user_service/service/AuthServiceTest.java
+++ user-service/src/test/java/com/fanbo/user_service/service/AuthServiceTest.java
@@ -2,6 +2,7 @@ package com.fanbo.user_service.service;
 
 import com.fanbo.user_service.Security.JwtUtils;
 import com.fanbo.user_service.client.MailServiceClient;
+import com.fanbo.user_service.dto.LoginRequest;
 import com.fanbo.user_service.dto.RegisterRequest;
 import com.fanbo.user_service.entity.User;
 import com.fanbo.user_service.event.OtpEventPublisher;
@@ -12,6 +13,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -38,6 +41,7 @@ public class AuthServiceTest {
     @InjectMocks
     private AuthService authService;
 
+    //REGISTER TESTS
     @Test
     public void register_shouldThrowException_whenEmailAlreadyExists() {
         //arrange
@@ -67,5 +71,50 @@ public class AuthServiceTest {
 
         //assert
         verify(userRepository).save(any(User.class));
+    }
+
+    @Test
+    public void register_shouldEncodePassword_whenPasswordIsNew() {
+        //arrange
+        RegisterRequest request = new RegisterRequest("test","testtest","test@gmail.com");
+        when(userRepository.existsByEmail(request.getEmail()))
+                .thenReturn(false);
+        //act
+        authService.register(request);
+
+        //assert
+        verify(passwordEncoder).encode(request.getPassword());
+    }
+
+    //LOGIN TESTS
+
+    @Test
+    public void login_shouldThrowException_whenPasswordDoesNotMatch() {
+        //arrange
+        LoginRequest request = new LoginRequest("test","testtest");
+        //tutaj wyjasnienie bo sie wywalilem
+        //testowac moja logike a nie framweork tak ja k wcenisj mielame niestety
+        //wrzucam dla frameworka cos nie wazne co ale ma wyrzucic bad credentials i esjt git
+        when(authenticationManager.authenticate(any()))
+                .thenThrow(BadCredentialsException.class);
+
+        //act and assert
+        //teraz sprawdzam loginservice czy sie logujac wyrzuci wyjatek z wyzej
+        //ktory powinno wyrzicic zawsze
+        assertThatThrownBy(() -> authService.login(request));
+    }
+
+    @Test
+    public void login_shouldPublishOtpEvent_whenEmailAndPasswordMatch() {
+        //arrange
+        LoginRequest request = new LoginRequest("test","testtest");
+
+        //act
+        //wywoluje loign
+        authService.login(request);
+
+        //assert
+        //spradzam czy sie odzywa publisher
+        verify(eventPublisher).publishOtpEvent(any());
     }
 }
